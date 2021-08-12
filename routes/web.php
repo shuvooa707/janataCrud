11fb070ae65341ef4307e9a066f23527d5e112a6
shuvo <shuvooa707@gmail.com>
2021-08-13 03:50:39 +0600

    ...

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [HomeController::class, 'show']);
+
+Route::post('/create', [HomeController::class, 'create']);
+Route::post('/remove', [HomeController::class, 'destroy']);
