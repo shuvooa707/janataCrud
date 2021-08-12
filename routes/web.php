9bf52ef610c8f888a2e00aa3e916a314e43b5b99
shuvo <shuvooa707@gmail.com>
2021-08-13 02:51:48 +0600

    first sqlModel commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'show']);
+
+Route::post('/create', [HomeController::class, 'create']);
+Route::post('/remove', [HomeController::class, 'destroy']);
